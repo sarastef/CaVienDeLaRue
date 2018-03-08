ae1524c6cd5df0f050e940e8cf0023331ce17f05
sarastef <a20132950@pucp.pe>
2018-03-08 17:50:19 +0100

    map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,7 +42,7 @@ angular.module('starter.controllers', [])
 .controller('homeCtrl', function($scope) {
   
 })
-
+/*
 .controller('MapController', function($scope, $ionicLoading) {
 
     google.maps.event.addDomListener(window, 'load', function() {
@@ -63,12 +63,66 @@ angular.module('starter.controllers', [])
                 map: map,
                 title: "My Location"
             });
+        }, function(){
+          console.log("erreur");
         });
 
         $scope.map = map;
     });
 
 })
+*/
+
+    .controller('MapController', function($scope, $ionicLoading, $compile) {
+      function initialize() {
+        var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+        
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+        
+        //Marker + infowindow + angularjs compiled ng-click
+        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+        var compiled = $compile(contentString)($scope);
+
+        var infowindow = new google.maps.InfoWindow({
+          content: compiled[0]
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Uluru (Ayers Rock)'
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+        });
+
+        $scope.map = map;
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+      
+      $scope.centerOnMe = function() {
+        if(!$scope.map) {
+          return;
+        }
+
+        navigator.geolocation.getCurrentPosition(function(pos) {
+          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          $scope.loading.hide();
+        }, function(error) {
+          alert('Unable to get location: ' + error.message);
+        });
+      };
+      
+      
+      
+    })
 
 .controller('MapTrajetController', function($scope, $ionicLoading) {
 
